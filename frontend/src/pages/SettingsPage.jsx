48c446d6b882fe4c1c8714e6bcc867c594fca5a6
Syed Zaheed <139610883+szaid47@users.noreply.github.com>
2024-12-24 03:56:32 +0530

    build_2

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,5 +1,5 @@
 import { THEMES } from "../constants";
-import { useThemeStore } from "../store/UseThemeStore";
+import { useThemeStore } from "../store/useThemeStore";
 import { Send } from "lucide-react";
 
 const PREVIEW_MESSAGES = [
